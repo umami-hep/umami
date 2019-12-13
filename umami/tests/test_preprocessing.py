dfd6025dc8b80b4cc412b2aabbc82e5dbc535326
Manuel Guth <guth.manuel@gmail.com>
2019-12-13 10:51:12 +0100

    adding additional tests

--- umami/tests/test_preprocessing.py
+++ umami/tests/test_preprocessing.py
@@ -30,22 +30,26 @@ class DownSamplingTestCase(unittest.TestCase):
         b_ind, c_ind, u_ind = down_s.GetIndices()
         assert len(b_ind) == len(df_zeros)
 
-    def test_out_of_binrange_underflow(self):
+    def test_underflow(self):
         df_minus_ones = pd.DataFrame(-1 * np.ones((1000, 2)),
                                      columns=["pt_uncalib", "abs_eta_uncalib"])
         down_s = DownSampling(df_minus_ones, df_minus_ones, df_minus_ones)
         b_ind, c_ind, u_ind = down_s.GetIndices()
-        print(b_ind)
         assert b_ind.size == 0
         assert c_ind.size == 0
         assert u_ind.size == 0
 
-    def test_out_of_binrange_overflow(self):
-        df_minus_ones = pd.DataFrame(1e9 * np.ones((1000, 2)),
+    def test_overflow(self):
+        df_minus_ones = pd.DataFrame(1e10 * np.ones((1000, 2)),
                                      columns=["pt_uncalib", "abs_eta_uncalib"])
         down_s = DownSampling(df_minus_ones, df_minus_ones, df_minus_ones)
         b_ind, c_ind, u_ind = down_s.GetIndices()
-        print(b_ind)
         assert b_ind.size == 0
         assert c_ind.size == 0
         assert u_ind.size == 0
+
+    def test_equal_length(self):
+        down_s = DownSampling(self.df_bjets, self.df_cjets, self.df_ujets)
+        b_ind, c_ind, u_ind = down_s.GetIndices()
+        assert len(b_ind) == len(c_ind)
+        assert len(b_ind) == len(u_ind)
