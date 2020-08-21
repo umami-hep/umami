7c7db5a8dad372b184ce6d0f4dd3a2c33a9888c3
Manuel Guth <manuel.guth@cern.ch>
2020-08-21 14:12:18 +0000

    small fix in setup script

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 setup(name='umami',
       version='0.0.0',  # Also change in module
       packages=["umami", "umami.tests", "umami.train_tools", "umami.tools",
-                "umami.preprocessing_tools"],
+                "umami.preprocessing_tools", "umami.tools.PyATLASstyle"],
       install_requires=["h5py",
                         "numpy",
                         "matplotlib",
