5357cec4cfe7951377b93d2656933b66623937b7
alfroch <alexander.froch@cern.ch>
2021-01-11 14:50:22 +0100

    Update url from the repo and adding the plotting_umami.py. Also some formatting of the code

--- setup.py
+++ setup.py
@@ -9,25 +9,34 @@ to install the package.
 from setuptools import setup
 
 
-setup(name='umami',
-      version='0.0.0',  # Also change in module
-      packages=["umami", "umami.tests", "umami.train_tools", "umami.tools",
-                "umami.preprocessing_tools", "umami.tools.PyATLASstyle",
-		"umami.evaluation_tools"],
-      #install_requires=["h5py",
-       #                 "numpy",
-        #                "matplotlib",
-         #               "seaborn",
-          #              "tables",
-           #             "pandas",
-                        #"tensorflow",
-                        #"keras"
-            #            ],
-      include_package_data=True,
-      test_suite='umami.tests',
-      scripts=["umami/preprocessing.py",
-               "umami/train_DL1.py", "umami/train_Dips.py",
-               "umami/train_umami.py", "umami/evaluate_model.py"],
-      description='Machine learning based flavour tagging training framework.',
-      url="https://gitlab.cern.ch/mguth/umami"
-      )
+setup(
+    name='umami',
+    version='0.0.0',  # Also change in module
+    packages=[
+        "umami", "umami.tests", "umami.train_tools", "umami.tools",
+        "umami.preprocessing_tools", "umami.tools.PyATLASstyle",
+        "umami.evaluation_tools"
+    ],
+    # install_requires=[
+    #     "h5py",
+    #     "numpy",
+    #     "matplotlib",
+    #     "seaborn",
+    #     "tables",
+    #     "pandas",
+    #     "tensorflow",
+    #     "keras"                 
+    # ],
+    include_package_data=True,
+    test_suite='umami.tests',
+    scripts=[
+        "umami/preprocessing.py",
+        "umami/train_DL1.py",
+        "umami/train_Dips.py",
+        "umami/train_umami.py",
+        "umami/evaluate_model.py",
+        "umami/plotting_umami.py"
+    ],
+    description='Machine learning based flavour tagging training framework.',
+    url="https://gitlab.cern.ch/atlas-flavor-tagging-tools/algorithms/umami"
+)
